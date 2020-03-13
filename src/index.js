65da7de18d42b6dd9593a0f436300fa286a8f129
Hola <hola131098@gmail.com>
2020-03-13 23:48:50 +0400

    feat: task expression-calculator upload

--- src/index.js
+++ src/index.js
@@ -1,10 +1,28 @@
 function eval() {
-    // Do not use eval!!!
+    // 60 + 29 / 57 - 85
     return;
 }
 
 function expressionCalculator(expr) {
-    // write your solution here
+     let left = [],
+        right = [];
+    let arr = expr.split('');
+    for (i = 0; i <= arr.length - 1; i++) {
+        if (arr[i] === '(') {
+            left.push(arr[i]);
+        }
+        if (arr[i] === ')') {
+            right.push(arr[i]);
+        }
+        if ((arr[i] + arr[i + 1] + arr[i + 2]) === '/ 0') {
+            throw new Error('TypeError: Division by zero.')
+        }
+    }
+    if (left.length === right.length) {
+        return new Function('return ' + expr)();
+    } else {
+        throw ("ExpressionError: Brackets must be paired");
+    }
 }
 
 module.exports = {
